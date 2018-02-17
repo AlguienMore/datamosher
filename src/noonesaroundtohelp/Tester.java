b8da3e14cf5c5bd0c3ed3861fa142467a764b32f
Personal <Personal@Personal-PC>
2018-02-17 18:17:01 -0500

    New Try with BufferedI/OStream

--- src/noonesaroundtohelp/Tester.java
+++ src/noonesaroundtohelp/Tester.java
@@ -4,22 +4,17 @@ import java.io.*;
 
 public class Tester {
 
-	public static void main(String args[]) {
+	public static void main(String args[]) throws IOException {
 		File image1 = null;
-		BufferedReader copy = null;
+		File image2 = new File("");
+		//FileReader fileReader = new FileReader(image2);
+		FileInputStream fis = new FileInputStream(image1);
+		BufferedInputStream copy = null;
+		FileOutputStream fos = new FileOutputStream(image2);
+		BufferedOutputStream paste = new BufferedOutputStream(fos);
 		
 		
-		try {
-			image1 = new File("C:\\Users\\josem\\Documents\\Artwork i liked\\5c7.jpg");
-			copy =  new BufferedReader(new FileReader(image1));
-			
-			
-			
-		}catch(IOException ioe){
-			System.out.println("Is anybody there? \n >" + ioe.getMessage());
-			
-		}
-		                        
+		
 
 	}
 
